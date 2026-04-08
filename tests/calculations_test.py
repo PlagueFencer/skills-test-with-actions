cc28dbb648fb2789c8cbae8fd93d7383442eedcc
Pavlo Polish <64736874+PlagueFencer@users.noreply.github.com>
2026-04-08 09:55:22 +0000

    Fix Fibonacci test case for n=10 to assert correct value

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -1,6 +1,7 @@
 # System Modules
 import sys
 import os
+from unittest import result
 
 # Installed Modules
 import pytest
@@ -59,12 +60,12 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
-    """Test with n=10."""
-    # Arrange
-    n = 10
+   """Test with n=10."""
+   # Arrange
+   n = 10
 
-    # Act
-    result = get_nth_fibonacci(n)
+   # Act
+   result = get_nth_fibonacci(n)
 
-    # Assert
-    assert result == 89
+   # Assert
+   assert result == 55
